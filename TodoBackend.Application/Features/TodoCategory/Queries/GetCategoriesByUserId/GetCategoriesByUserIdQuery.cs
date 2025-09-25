f0f05e07ff243972514e726ecc577e47ef83ce89
Mustafa Gunaltay <mustafa-gunaltay@hotmail.com>
2025-09-25 04:38:37 +0300

    feat(api): add GetCategoriesByUserId endpoint including proper application layer

---
+++ TodoBackend.Application/Features/TodoCategory/Queries/GetCategoriesByUserId/GetCategoriesByUserIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TodoBackend.Application.Features.BuildingBlocks;
+using TodoBackend.Application.ViewModels;
+
+namespace TodoBackend.Application.Features.TodoCategory.Queries.GetCategoriesByUserId;
+
+public record GetCategoriesByUserIdQuery(int UserId) : IRequest<Result<IReadOnlyList<CategoryViewModel>>>;
+
+
---
+++ TodoBackend.Application/Features/TodoCategory/Queries/GetCategoriesByUserId/GetCategoriesByUserIdQueryValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoBackend.Application.Features.TodoCategory.Queries.GetCategoriesByUserId;
+
+public class GetCategoriesByUserIdQueryValidator : AbstractValidator<GetCategoriesByUserIdQuery>
+{
+    public GetCategoriesByUserIdQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .GreaterThan(0).WithMessage("UserId must be greater than 0.");
+    }
+}
